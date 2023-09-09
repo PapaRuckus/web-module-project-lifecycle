73a1bc3e8d1a9e3d0e2866eb25cc674f743d67c9
Calvin K <calvinknaub@gmail.com>
2023-09-09 12:03:05 -0700

    handled fetchAllTodos, made some changes to my return in App

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -8,14 +8,17 @@ export default class App extends React.Component {
     super();
     this.state = {
       todos: [],
+      error: "",
     };
   }
 
   fetchAllTodos = () => {
     axios
       .get(URL)
-      .then((res) => this.setState({...this.state, todos: res.data.data}))
-      .catch((err) => console.error(err));
+      .then((res) => this.setState({ ...this.state, todos: res.data.data }))
+      .catch((err) =>
+        this.setState({ ...this.state, error: err.response.data.message })
+      );
   };
 
   componentDidMount() {
@@ -25,21 +28,19 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        <div id="error">Error: No Error here</div>
+        <div id="error">{this.state.error}</div>
         <div id="todos">
           <h2>Todos:</h2>
-          {
-            this.state.todos.map((td) => {
-              return <div key={td.id}>{td.name}</div>
-            })
-          }
+          {this.state.todos.map((td) => {
+            return <div key={td.id}>{td.name}</div>;
+          })}
         </div>
         <form id="todoForm">
           <input type="text" placeholder="Type Todo"></input>
-          <input type='submit'></input>
+          <input type="submit"></input>
           <button>Clear Completed</button>
         </form>
       </div>
-    )
+    );
   }
 }

a5d6a3424035917f3d0c1eaa83be3e5640a45533
Calvin K <calvinknaub@gmail.com>
2023-09-11 12:44:20 -0700

    made the ftodolist props

--- frontend/components/TodoList.js
+++ frontend/components/TodoList.js
@@ -2,6 +2,20 @@ import React from 'react'
 
 export default class TodoList extends React.Component {
   render() {
-    return null
+    const { todos, displayCompleteds, toggleCompleted } = this.props;
+    return (
+      <div id="todos">
+        <h2>Todos:</h2>
+        {todos.reduce((acc, td) => {
+          if (displayCompleteds || !td.completed)
+            return acc.concat(
+              <div onClick={toggleCompleted(td.id)} key={td.id}>
+                {td.name} {td.completed ? " ✔️" : ""}
+              </div>
+            );
+          return acc;
+        }, [])}
+      </div>
+    );
   }
 }
